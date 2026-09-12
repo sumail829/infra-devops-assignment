6e1717e7a7b0f04c3e3d3866058c219b89aff0a3
sumail829 <samirpanjiyar4@gmail.com>
2026-09-12 03:21:14 +0000

    added:Compressed zip backup

--- scripts/db_backup.sh
+++ scripts/db_backup.sh
@@ -2,11 +2,11 @@
 
 BACKUP_DIR="/var/backups/db"
 DATE=$(date +%Y%m%d)
-BACKUP_FILE="$BACKUP_DIR/db_backup_$DATE.sql"
+BACKUP_FILE="$BACKUP_DIR/db_backup_$DATE.sql.gz"
 
 mkdir -p "$BACKUP_DIR"
 
-docker exec postgres pg_dump -U appuser appdb > "$BACKUP_FILE"
+docker exec postgres pg_dump -U appuser appdb | gzip > "$BACKUP_FILE"
 
 if [ $? -eq 0 ]; then
     echo "Backup successful: $BACKUP_FILE"
